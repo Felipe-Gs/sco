ee603d9ba5756261542432cab19c0fc49036ff8a
Felipe-Gs <felipegs2905@gmail.com>
2023-06-14 13:21:49 -0300

    Refactor UniqueCpfValidator

--- openodonto-web/src/main/java/br/ueg/openodonto/validator/UniqueCpfValidator.java
+++ openodonto-web/src/main/java/br/ueg/openodonto/validator/UniqueCpfValidator.java
@@ -1,9 +1,5 @@
 package br.ueg.openodonto.validator;
 
-import java.lang.reflect.Field;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -21,58 +17,73 @@ import br.ueg.openodonto.controle.ManageBeanGeral;
 import br.ueg.openodonto.dominio.constante.PessoaFisica;
 import br.ueg.openodonto.util.WordFormatter;
 
-public class UniqueCpfValidator<T extends Entity & PessoaFisica<T>> extends AbstractValidator implements ObjectValidatorType{	
-	
-	public UniqueCpfValidator(T value) {
-		this(null,value);
-	}
-	
-	public UniqueCpfValidator(CpfValidator next,T value) {
-		super(next, value);
-	}
-	
-	@Override
-	@SuppressWarnings("unchecked")
-	public T getValue() {
-		return (T) super.getValue();
-	}
-	
-	@Override
-	@SuppressWarnings("unchecked")
-	protected boolean validate() {
-		Class<T> type = (Class<T>) getValue().getClass();
-		String cpf = WordFormatter.clear(getValue().getCpf());
-		EntityManager<T> dao = (EntityManager<T>) ManageBeanGeral.getDao(type);	
-		Map<String,Object> params = new HashMap<String, Object>(1);
-		params.put("cpf", cpf);
-		List<Field> keyFields = OrmResolver.getKeyFields(new LinkedList<Field>(), type, true);
-		String[] fields = new String[keyFields.size()];
-		for(int i = 0;i < keyFields.size();i++){
-			fields[i] = keyFields.get(i).getName();
-		}
-		IQuery query = CrudQuery.getSelectQuery(getValue().getClass(), params, fields);
-		try {
-			List<Map<String,Object>> result = dao.getSqlExecutor().executarUntypedQuery(query);
-			if(result.size() == 1){
-				OrmFormat format = new OrmFormat(getValue());
-				Map<String,Object> already = result.get(0);
-				Map<String,Object> local = format.format(fields);
-				boolean isSamePf = true;
-				OrmTranslator translator = new OrmTranslator(keyFields);				
-				for(String field : fields){
-					isSamePf = isSamePf && already.get(translator.getColumn(field)).equals(local.get(field));
-				}
-				if(!isSamePf){
-					setErrorMsg("CPF já está sendo usado.");
-				}
-				return isSamePf;
-			}else if(result.size() > 1){
-				throw new IllegalStateException("Falha de integridade.Permitiso apenas uma pessoa por CPF.");
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return true;
-	}	
+public class UniqueCpfValidator<T extends Entity & PessoaFisica<T>> extends AbstractValidator implements ObjectValidatorType {
+    private static final String ERROR_MSG = "CPF já está sendo usado.";
 
+    public UniqueCpfValidator(T value) {
+        super(value);
+    }
+
+    @Override
+    protected boolean validate() {
+        T value = getValue();
+        String cpf = WordFormatter.clear(value.getCpf());
+        UniqueCpfChecker<T> checker = new UniqueCpfChecker<>(value.getClass(), cpf);
+        return checker.check();
+    }
+
+    private static class UniqueCpfChecker<T extends Entity & PessoaFisica<T>> {
+        private final Class<T> type;
+        private final String cpf;
+
+        public UniqueCpfChecker(Class<T> type, String cpf) {
+            this.type = type;
+            this.cpf = cpf;
+        }
+
+        public boolean check() {
+            EntityManager<T> dao = ManageBeanGeral.getDao(type);
+            List<Map<String, Object>> result = getQueryResult(dao);
+            if (result.size() == 1) {
+                return isSamePf(result.get(0));
+            } else if (result.size() > 1) {
+                throw new IllegalStateException("Falha de integridade. Permitido apenas uma pessoa por CPF.");
+            }
+            return true;
+        }
+
+        private List<Map<String, Object>> getQueryResult(EntityManager<T> dao) {
+            IQuery query = createQuery(dao);
+            try {
+                return dao.getSqlExecutor().executarUntypedQuery(query);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        private IQuery createQuery(EntityManager<T> dao) {
+            Map<String, Object> params = Map.of("cpf", cpf);
+            List<String> keyFieldNames = OrmResolver.getKeyFieldNames(type, true);
+            String[] fields = keyFieldNames.toArray(new String[0]);
+            return CrudQuery.getSelectQuery(type, params, fields);
+        }
+
+        private boolean isSamePf(Map<String, Object> already) {
+            T value = getValue();
+            OrmFormat format = new OrmFormat(value);
+            List<String> keyFieldNames = OrmResolver.getKeyFieldNames(type, true);
+            Map<String, Object> local = format.format(keyFieldNames.toArray(new String[0]));
+            OrmTranslator translator = new OrmTranslator(type, keyFieldNames);
+            for (String fieldName : keyFieldNames) {
+                Object alreadyValue = already.get(translator.getColumn(fieldName));
+                Object localValue = local.get(fieldName);
+                if (!alreadyValue.equals(localValue)) {
+                    setErrorMsg(ERROR_MSG);
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
 }
